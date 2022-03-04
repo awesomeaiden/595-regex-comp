83c71e94801db0bd5bcac970151a004d770b6865
Josh Majors <jdomajors@gmail.com>
2022-03-03 20:15:11 -0500

    tested functionality for sending image from webserver to client

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 import os
 import sys
+import base64
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,10 +13,19 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/method1")
+def generate_regex_visualization_method1():
+    regex_str = request.args['str']
+    base64arr = None
+    with open("test_img.png", 'rb') as f:
+        byte_arr = f.read()   
+        base64arr = base64.standard_b64encode(byte_arr)
+    return base64arr
+
 def main():
     if len(sys.argv) != 3:
-        print("Usage: python3 app.py <ip> <port>")
-        
+        print("Error, usage: python app.py <ip> <port>")
+
     ip = sys.argv[1]
     port = int(sys.argv[2])
 
