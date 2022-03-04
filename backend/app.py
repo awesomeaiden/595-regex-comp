e6d4c0b79e927d766134f132702ca7a6a4187fdb
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2022-03-03 22:02:10 -0500

    Reformat for PEP

--- backend/app.py
+++ backend/app.py
@@ -8,18 +8,21 @@ load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
 
 @app.route("/method1")
 def generate_regex_visualization_method1():
     regex_str = request.args['str']
     base64arr = None
     with open("test_img.png", 'rb') as f:
-        byte_arr = f.read()   
+        byte_arr = f.read()
         base64arr = base64.standard_b64encode(byte_arr)
     return base64arr
+
 
 def main():
     if len(sys.argv) != 3:
@@ -29,6 +32,7 @@ def main():
     port = int(sys.argv[2])
 
     app.run(host=ip, port=port)
+
 
 if __name__ == '__main__':
     main()
